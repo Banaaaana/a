ecced83696f7a545bf54187847cb5a96f57ce2bc
unknown <lkunze@qq.com>
2020-11-21 21:13:58 +0800

    perfect

--- 1.c
+++ 1.c
@@ -52,17 +52,26 @@ int main()
         scanf("%d", &back0);
             break;
         case 5:
-            delete (&list, list.length);
+            //if (Insert(&list, temp1)) {
+            //printf("----插入成功----\n");
+            //list->length += 1;
+            //}
             break;
         case 6:
-            exit0 = 1;
+            delete (&list, list.length);
             break;
-/*        case 7:
-            add_emements(&list);
-            break;*/
+            /*case 7:
+            Statistics(&list, temp1);
+            break;
+        case 8:
+            exit = 1;
+            break;
+        */
         default:
             break;
         }
+        //addmember();
+        //delete();
     };
 }
 
@@ -73,10 +82,10 @@ void start_list(list_type *list)
 
 void menu()
 {
-    printf("-------------------不讲武德医院医生信息簿------------------\n");
+    printf("-------------------大铁棍子医院医生信息簿------------------\n");
     printf("--------------------------帮助--------------------------\n");
     printf("1.添加医生信息\t2.显示医生信息\t3.查找医生\t4.查找房间号\n");
-    printf("4.删除医生信息\t5.统计医生人数\t6.退出\t\n");
+    printf("5.插入医生信息\t6.删除医生信息\t7.统计医生人数\t\t8.退出\n");
     printf("------------------------------------------------------------\n");
 };
 
@@ -100,27 +109,6 @@ void create_list(list_type *list, int list_length)
     scanf("%d", &back0);
 };
 
-/*void add_emements(list_type *list)
-{
-    list->length=20;
-     for (int i = 1; i < 21; i++)
-     {
-         switch (i)
-         {
-         case 1:
-             strcpy(list->data[i].name, &name0[10]);
-             break;
-        case 2:
-             strcpy(list->data[i].name, &name1[10]);
-             break;
-         default:
-             break;
-         }
-        
-     }
-
-};*/
-
 void showlist(list_type *list, int list_length)
 {
     int i;
@@ -129,11 +117,10 @@ void showlist(list_type *list, int list_length)
         printf("顺序表长度错误!\n");
         return;
     }
-    printf("--------不讲武德医院医生信息簿--------\n");
+    printf("--------大铁棍子医院医生信息簿--------\n");
     printf("姓名\t房间号\t评级\t就职时间\n");
     for (i = 0; i < list_length; i++)
         printf("%s\t%s\t%s\t%s\n", list->data[i].name, list->data[i].room, list->data[i].grade, list->data[i].time);
-        printf("-------------------");
     printf("按任意键返回主菜单");
     scanf("%d", &back0);
 };
@@ -192,22 +179,22 @@ int look_room(list_type *list, int list_length)
     return 0;
 };
 
-
+void addmember();
 void delete (list_type *list, int list_length)
 {
     int i_number_conclusion, a;
     i_number_conclusion = look_name(list, list_length);
     if (i_number_conclusion != 0)
     {
-        printf("输入1来确认删除该医生的信息，输入0取消");
+        printf("输入-1来确认删除该医生的信息，输入0取消");
         scanf("%d", &a);
-        if (a == 1)
+        if (a == -1)
         {
             int i = i_number_conclusion;
-            strcpy(list->data[i - 1].name, list->data[i].name);
-            strcpy(list->data[i - 1].room, list->data[i].room);
-            strcpy(list->data[i - 1].grade, list->data[i].grade);
-            strcpy(list->data[i - 1].time, list->data[i].time);
+            strcpy(list->data[i].name, list->data[i+1].name);
+            strcpy(list->data[i].room, list->data[i+1].room);
+            strcpy(list->data[i].grade, list->data[i+1].grade);
+            strcpy(list->data[i].time, list->data[i+1].time);
             list->length--;
             printf("删除成功");
             showlist(list, list->length);
@@ -221,11 +208,10 @@ void delete (list_type *list, int list_length)
             return;
         }
     }
-
     else
     {
         printf("请返回");
         printf("按任意键返回主菜单");
         scanf("%d", &back0);
     }
-};
+}
